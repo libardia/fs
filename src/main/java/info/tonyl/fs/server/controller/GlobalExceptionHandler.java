b44c950cda9f1fe419b6c5849dc2dda01b8b2e03
Anthony Libardi <libardi42@gmail.com>
2019-07-19 19:56:00 -0500

    Just adding response body fixed it.

--- src/main/java/info/tonyl/fs/server/controller/GlobalExceptionHandler.java
+++ src/main/java/info/tonyl/fs/server/controller/GlobalExceptionHandler.java
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import info.tonyl.fs.server.responses.ErrorResponse;
 
 @ControllerAdvice
+@ResponseBody
 public class GlobalExceptionHandler {
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
