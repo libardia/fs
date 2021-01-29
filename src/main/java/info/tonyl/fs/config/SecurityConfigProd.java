46b5692bdb186c4e92a73206738774327913b52b
Anthony Libardi <libardi42@gmail.com>
2021-01-28 18:30:16 -0600

    Add https to download link

--- src/main/java/info/tonyl/fs/config/SecurityConfigProd.java
+++ src/main/java/info/tonyl/fs/config/SecurityConfigProd.java
@@ -1,7 +1,6 @@
 package info.tonyl.fs.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,9 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +19,6 @@ public class SecurityConfigProd extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	PasswordEncoder encoder;
-
-//	@Bean
-//	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//		http.redirectToHttps();
-//		return http.build();
-//	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
